3c47cb7787cfa3853b57e578194a144684eae73b
Emir Ay <106332223+emirayemr@users.noreply.github.com>
2026-01-12 15:22:41 +0300

    Implement minTimeToVisitAllPoints function

---
+++ minTimeToVisitAllPoints.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        total = 0
+        for i in range(1, len(points)):
+            dx = abs(points[i][0] - points[i-1][0])
+            dy = abs(points[i][1] - points[i-1][1])
+            total += max(dx, dy)
+        return total
+
+        
